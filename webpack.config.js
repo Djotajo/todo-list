2504587564592cf46c39bc8dd41a78bf6206f4d9
Djotajo <20823564+Djotajo@users.noreply.github.com>
2023-11-06 17:18:23 +0100

    Config Babel

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,16 @@ module.exports = {
         test: /\.(jpg|png|gif|svg)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
     ],
   },
 };
